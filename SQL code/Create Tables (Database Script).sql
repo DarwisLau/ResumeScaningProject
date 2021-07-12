d524e13154bc7c8fe4ea4c00b117e994d64ecee3
ChanKhaiShen <77774878+ChanKhaiShen@users.noreply.github.com>
2021-07-12 22:49:53 +0800

    Some changes
    - Changed character limit and NULL/NOT NULL settings of a few columns

--- SQL code/Create Tables (Database Script).sql
+++ SQL code/Create Tables (Database Script).sql
@@ -1,21 +1,23 @@
 CREATE TABLE Applicant (
 applicantID int NOT NULL AUTO_INCREMENT, 
 applicantICNumber char(12) UNIQUE NOT NULL,
-applicantName varchar(200) NOT NULL,
+applicantName varchar(150) NOT NULL,
 applicantEmail varchar(150) UNIQUE NOT NULL,
 applicantContactNumber varchar(11) UNIQUE NOT NULL,
-applicantLanguageWritten varchar(500) NULL,
-applicantLanguageSpoken varchar(500) NULL,
-applicantProgrammingLanguage varchar(500) NULL,
-applicantHighestEducation varchar(800) NULL,
-applicantPastWorkExperience varchar(3000) NULL,
-applicantPastWorkDuration varchar(300) NULL,
-applicantSoftSkills varchar(1500) NULL);
+applicantLanguageWritten varchar(100) NOT NULL,
+applicantLanguageSpoken varchar(100) NOT NULL,
+applicantProgrammingLanguage varchar(100) NULL,
+applicantHighestEducation varchar(100) NOT NULL,
+applicantPastWorkExperience varchar(500) NOT NULL,
+applicantPastWorkDuration varchar(30) NOT NULL,
+applicantSoftSkill varchar(100) NOT NULL);
+
 
 CREATE TABLE Application (
-positionApplied varchar(200) NOT NULL,
+positionApplied varchar(50) NOT NULL,
 isActive bit(1) NOT NULL DEFAULT 1,
 applicantID int UNIQUE NOT NULL);
+
 
 
 ALTER TABLE Applicant
