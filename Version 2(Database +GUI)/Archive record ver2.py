f6f06c5673414d4efbff2de43fc2e8d6c5dab89f
ChanKhaiShen <77774878+ChanKhaiShen@users.noreply.github.com>
2021-07-19 15:45:11 +0800

    Some changes

--- Version 2(Database +GUI)/Archive record ver2.py
+++ Version 2(Database +GUI)/Archive record ver2.py
@@ -1,159 +1,16 @@
 from tkinter import*
 from tkinter.font import Font
+from tkinter import messagebox
 
+import mysql.connector
+#mysql.conector is a driver to access mySQL database.
+#It is used to select applicant's information from the database and change applicant's archive status in the database.
+#Source: It is installed together when installing the mySQL software using mySQL installer. Source of the mySQL installer: https://dev.mysql.com/downloads/installer/
 
-
-#Function executed when Search Button is pressed
-def selectOneFromAllRecords():
-
-    """Function to select an applicant's information from database and return the information to the applicant info section.
-       This function does not have specific input parameters, but the function will get the applicant's IC number from the IC search panel (expected string).
-       Output is either applicant's information (displayed in applicant info section), or a message box that shows the error message (if there is any error message) from the database software. If the applicant does not exist in the
-       database, the applicant info section will be left blank. If the applicant's programming language column is empty, the line for programming language will be left blank."""
-
-
-    #Set the variables
-    dataICNumber = entryIC.get()
-    from ValidateData_ver2 import standardise_applicantICNumber
-    #ValidateData contains code for data validation.
-    #It is used to remove "-" or space from IC number before it is used to query applicant's information in the database.
-    #Source: Part of this project
-    dataICNumber = standardise_applicantICNumber (dataICNumber)
-    dataName = ""
-    dataEmail = ""
-    dataContactNumber = ""
-    dataPositionApplied = ""
-    dataLanguageWritten = ""
-    dataLanguageSpoken = ""
-    dataProgrammingLanguage = ""
-    dataPastWorkExperience = ""
-    dataPastWorkDuration = ""
-    dataHighestEducation = ""
-    dataSoftSkill = ""
-    dataIsActive = ""
-
-
-    #Select from database
-    import mysql.connector
-    #mysql.conector is a driver to access mySQL database.
-    #It is used to query applicant's information in the database, and get the result of the query.
-    #Source: It is installed together when installing the mySQL software using mySQL installer. Source of the mySQL installer: https://dev.mysql.com/downloads/installer/
-    database = mysql.connector.connect (
-        host = "localhost",
-        user = "Resume_Scanning_Project_Admin",
-        password = "AbcdeF,123456!",
-        database = "ResumeScanningProject"
-    )
-    databaseCursor = database.cursor()
-    try:
-        result = databaseCursor.callproc ("SelectOneFromAllRecords", (dataICNumber, dataName, dataEmail, dataContactNumber, dataPositionApplied, dataLanguageWritten, dataLanguageSpoken, dataProgrammingLanguage, dataPastWorkExperience, dataPastWorkDuration, dataHighestEducation, dataSoftSkill, dataIsActive))
-
-        #Display the applicant's information in the applicant info section
-        if result[1] is None:
-            text_Name.set("")
-        else:
-            text_Name.set(result[1])
-        if result[2] is None:
-            text_Email.set("")
-        else:
-            text_Email.set(result[2])
-        if result[3] is None:
-            text_Contact.set("")
-        else:
-            text_Contact.set(result[3])
-        if result[4] is None:
-            text_Position.set("")
-        else:
-            text_Position.set(result[4])
-        if result[5] is None:
-            text_LangWrite.set("")
-        else:
-            text_LangWrite.set(result[5])
-        if result[6] is None:
-            text_LangSpoke.set("")
-        else:
-            text_LangSpoke.set(result[6])
-        if result[7] is None:
-            text_CodeLang.set("")
-        else:
-            text_CodeLang.set(result[7])
-        if result[8] is None:
-            text_WorkExp.set("")
-        else:
-            text_WorkExp.set(result[8])
-        if result[9] is None:
-            text_Duration.set("")
-        else:
-            text_Duration.set(result[9])
-        if result[10] is None:
-            text_HighestEdu.set("")
-        else:
-            text_HighestEdu.set(result[10])
-        if result[11] is None:
-            text_SoftSkill.set("")
-        else:
-            text_SoftSkill.set(result[11])
-        var.set(result[12])
-
-    except mysql.connector.Error as errorMessage:
-        from tkinter import messagebox
-        messagebox.showinfo("Error", errorMessage)
-    finally:
-        databaseCursor.close()
-        database.close()
-
-
-
-#Function executed when Save Button is pressed
-def changeArchiveStatus ():
-
-    """Function to change an applicant's archive status in the database.
-       This function does not have specific input parameters, but the functio will get the applicant's new archive status from the radio button.
-       Output is a message box that shows the message "Record status changed" if the change of archive status was successful, or promts error message if the change of archive status was not successful."""
-   
-
-    #Validate data and set the variables
-    from ValidateData import standardise_applicantICNumber
-    #ValidateData contains code for data validation.
-    #It is used to remove "-" and space from IC number and make sure that the other data is valid before it is updated in the database.
-    #Source: Part of this project
-    from tkinter import messagebox
-    
-    #IC number
-    dataICNumber = entryIC.get()
-    dataICNumber = standardise_applicantICNumber (dataICNumber)
-
-    #Archive status
-    dataIsActive = var.get()
-
-    outMessage = None
-
-
-    #Change archive status in database
-    import mysql.connector
-    #mysql.conector is a driver to access mySQL database.
-    #It is used to update applicant's information in the database, and check whether the update was successful or not.
-    #Source: It is installed together when installing the mySQL software using mySQL installer. Source of the mySQL installer: https://dev.mysql.com/downloads/installer/
-    database = mysql.connector.connect (
-        host = "localhost",
-        user = "Resume_Scanning_Project_Admin",
-        password = "AbcdeF,123456!",
-        database = "ResumeScanningProject"
-    )
-    databaseCursor = database.cursor()
-    try:
-        returnMessage = databaseCursor.callproc ("ChangeArchiveStatus", (dataICNumber, dataIsActive, outMessage))
-        database.commit()
-        if returnMessage[2] == "Record status changed":
-            messagebox.showinfo("", returnMessage[2])
-        else:
-            messagebox.showinfo("Error", returnMessage[2])
-    except mysql.connector.Error as errorMessage:
-        messagebox.showinfo("Error", errorMessage)
-    finally:
-        databaseCursor.close()
-        database.close()
-
+from ValidateData import standardise_applicantICNumber, validate_applicantICNumber
+#ValidateData contains code for data validation.
+#It is used to make sure that IC number is valid before it is used to select applicant's information from the database or change applicant's archive status in the database.
+#Source: Part of this project
 
 
 #Window size, title
@@ -164,8 +21,7 @@ root.title("Archive Record")
 
 #Define image for Background and button
 bg= PhotoImage(file='D:/GUI Image/background.png')
-SaveButtonImg =PhotoImage(file='D:/GUI Image/update button.png')
-BackToMenuButtonImg =PhotoImage(file='D:/GUI Image/Back to menu.png')
+UpdateButtonImg =PhotoImage(file='D:/GUI Image/update button.png')
 ActCheckboxImg= PhotoImage(file='D:/GUI Image/active.png')
 InactCheckboxImg=PhotoImage(file='D:/GUI Image/inactive.png')
 SearchButtonImg = PhotoImage(file='D:/GUI Image/search button.png')
@@ -198,6 +54,58 @@ Header.create_text(140,85,anchor="nw", text="ALL Project by Muhammad Darwis and 
 my_canvas.create_text(140,203,anchor="nw", text="Search Applicant Using IC No.:", font=FormFont)
 my_canvas.create_text(140,254,anchor="nw", text="Applicant IC No. :", font=FillFont)
 
+#Function executed when Search Button is pressed
+def selectOneFromAllRecords():
+
+    """Function to select an applicant's information from database and return the information to the applicant info section.
+       This function does not have specific input parameters, but the function will get the applicant's IC number from the IC search panel (expected string).
+       Output is either applicant's information (displayed in applicant info section), or a message box that shows any error message."""
+
+    #Clear applicant info section
+    textList = [text_Name, text_Email, text_Contact, text_Position, text_LangWrite, text_LangSpoke, text_CodeLang, text_WorkExp, text_Duration, text_HighestEdu, text_SoftSkills]
+    for text in textList:
+        text.set("")
+    var.set(0)
+    
+    #Get and validate IC number
+    dataICNumber = entryIC.get()
+    dataICNumber = standardise_applicantICNumber (dataICNumber)
+    errorMessage = validate_applicantICNumber (dataICNumber)
+    if errorMessage is not None:
+        from tkinter import messagebox
+        messagebox.showinfo("Error", errorMessage)
+        return
+
+    #Select from database
+    database = mysql.connector.connect (
+        host = "localhost",
+        user = "Resume_Scanning_Project_Admin",
+        password = "AbcdeF,123456!",
+        database = "ResumeScanningProject"
+    )
+    databaseCursor = database.cursor()
+    try:
+        databaseCursor.callproc ("SelectOneFromAllRecords", (dataICNumber,))
+
+        #Display the applicant's information in applicant info section
+        index = 0
+        for result in databaseCursor.stored_results():
+            for row in result.fetchall():
+                while index <= 10:
+                    textList[index].set(row[index])
+                    index += 1
+                if row[11] == 1:
+                    var.set(1)
+                else:
+                    var.set(2)
+
+    except mysql.connector.Error as errorMessage:
+        from tkinter import messagebox
+        messagebox.showinfo("Error", errorMessage)
+    finally:
+        databaseCursor.close()
+        database.close()
+
 #Search Button
 img_label = Label(image=SearchButtonImg)
 ButtonSave = Button(root, image=SearchButtonImg, borderwidth=0,highlightthickness=0,bd=0,command=selectOneFromAllRecords)
@@ -214,7 +122,8 @@ my_canvas.create_text(140,639,anchor="nw", text="Programming Language:", font=Fi
 my_canvas.create_text(140,694,anchor="nw", text="Past Work Experience:", font=FillFont)
 my_canvas.create_text(880,694,anchor="nw", text="Duration:", font=FillFont)
 my_canvas.create_text(140,749,anchor="nw", text="Highest Education:", font=FillFont)
-my_canvas.create_text(140,804,anchor="nw", text="Applicant Status:", font=FillFont)
+my_canvas.create_text(140,859,anchor="nw", text="Applicant Status:", font=FillFont)
+my_canvas.create_text(140,804,anchor="nw",text="Soft Skills:", font=FillFont)
 
 #Form fill
 entryIC = Entry(root, width = 100)
@@ -231,7 +140,7 @@ text_CodeLang = StringVar()
 text_WorkExp = StringVar()
 text_Duration = StringVar()
 text_HighestEdu = StringVar()
-text_SoftSkill = StringVar()
+text_SoftSkills = StringVar()
 
 #Display Applicant Info space
 query_Label_Name = Label(root, width=85, textvariable=text_Name)
@@ -264,10 +173,58 @@ my_canvas.create_window(1040, 704, anchor="nw", window=query_Label_Duration)
 query_Label_HighestEdu = Label(root, width = 85, textvariable=text_HighestEdu)
 my_canvas.create_window(500, 762, anchor="nw", window=query_Label_HighestEdu)
 
-#Save Button
-img_label = Label(image=SaveButtonImg)
-entryButtonSave = Button(root, image=SaveButtonImg, borderwidth=0, highlightthickness=0, bd=0, command=changeArchiveStatus)
-my_canvas.create_window(140, 858, anchor="nw", window=entryButtonSave)
+query_Label_SoftSkills = Label(root, width = 85, textvariable=text_SoftSkills)
+my_canvas.create_window(500, 810, anchor="nw", window=query_Label_SoftSkills)
+
+#Function executed when Save Button is pressed
+def changeArchiveStatus ():
+
+    """Function to change applicant's archive status in the database.
+       This function does not have specific input parameters, but the function will get the applicant's IC number from the IC search panel (expected string) and new archive status from the radio button.
+       Output is a message box that shows the message "Record status changed" if the change of archive status was successful, or a message box that promts error message if the change of archive status was not successful."""
+
+    #Get IC number and new archive status, validate IC number and set the variables
+    dataICNumber = entryIC.get()
+    dataICNumber = standardise_applicantICNumber (dataICNumber)
+    errorMessage = validate_applicantICNumber (dataICNumber)
+    if errorMessage is not None:
+        messagebox.showinfo("Error", errorMessage)
+        return
+    dataVar = var.get()
+    if dataVar == 1:
+        dataIsActive = 1
+    elif dataVar == 2:
+        dataIsActive = 0
+    else:
+        messagebox.showinfo("Error", "Applicant status is not chosen.")
+        return
+    outMessage = None
+
+    #Change archive status in database
+    database = mysql.connector.connect (
+        host = "localhost",
+        user = "Resume_Scanning_Project_Admin",
+        password = "AbcdeF,123456!",
+        database = "ResumeScanningProject"
+    )
+    databaseCursor = database.cursor()
+    try:
+        returnMessage = databaseCursor.callproc ("ChangeArchiveStatus", (dataICNumber, dataIsActive, outMessage))
+        database.commit()
+        if returnMessage[2] == "Record status changed":
+            messagebox.showinfo("", returnMessage[2])
+        else:
+            messagebox.showinfo("Error", returnMessage[2])
+    except mysql.connector.Error as errorMessage:
+        messagebox.showinfo("Error", errorMessage)
+    finally:
+        databaseCursor.close()
+        database.close()
+
+#Update Button
+img_label = Label(image=UpdateButtonImg)
+entryButtonUpdate = Button(root, image=UpdateButtonImg, borderwidth=0, highlightthickness=0, bd=0, command=changeArchiveStatus)
+my_canvas.create_window(140, 858, anchor="nw", window=entryButtonUpdate)
 
 
 #RadioButton to change applicant status
@@ -276,8 +233,9 @@ var = IntVar()
 ActiveCheck=Radiobutton(root, image=ActCheckboxImg, variable=var, value=1, borderwidth=0, highlightthickness=0,bd=0)
 my_canvas.create_window(471,804, anchor="nw", window=ActiveCheck)
 
-InActiveCheck=Radiobutton(root, image=InactCheckboxImg, variable=var,value=0,borderwidth=0, highlightthickness=0,bd=0)
+InActiveCheck=Radiobutton(root, image=InactCheckboxImg, variable=var,value=2,borderwidth=0, highlightthickness=0,bd=0)
 my_canvas.create_window(630,804, anchor="nw", window=InActiveCheck)
+
 
 
 root.mainloop()
