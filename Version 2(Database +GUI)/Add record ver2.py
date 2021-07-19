084bc5de04ab44f56eba5388891f7f1ac72e5452
ChanKhaiShen <77774878+ChanKhaiShen@users.noreply.github.com>
2021-07-19 12:52:22 +0800

    Some changes

--- Version 2(Database +GUI)/Add record ver2.py
+++ Version 2(Database +GUI)/Add record ver2.py
@@ -1,90 +1,16 @@
 from tkinter import*
 from tkinter.font import Font
+from tkinter import messagebox
 
+import mysql.connector
+#mysql.conector is a driver to access mySQL database.
+#It is used to insert new applicant's information into the database.
+#Source: It is installed together when installing the mySQL software using mySQL installer. Source of the mySQL installer: https://dev.mysql.com/downloads/installer/
 
-
-#Function executed when Save button is pressed
-def insertNewRecord ():
-
-    """Function to add new record into the database.
-       This function does not have specific input parameters, but the function will get the applicant's information from the form.
-       Output is a message box that shows the message "Record successfully added" if the insertion was successful, or promts error message if the insertion was not successful."""
-
-    
-    #Validate data and set the variables
-    from ValidateData import standardise_applicantICNumber, validate_applicantICNumber, standardise_applicantEmail, validate_applicantEmail, standardise_applicantContactNumber, validate_applicantContactNumber, validate_otherData
-    #ValidateData contains code for data validation.
-    #It is used to make sure that the data is valid before it is inserted into the database.
-    #Source: Part of this project
-    from tkinter import messagebox
-    
-    #IC number
-    dataICNumber = entryIC.get()
-    dataICNumber = standardise_applicantICNumber (dataICNumber)
-    errorMessage = validate_applicantICNumber (dataICNumber)
-    if errorMessage is not None:
-        messagebox.showinfo("Error", errorMessage)
-        return
-
-    #Email address
-    dataEmail = entryEmail.get()
-    dataEmail = standardise_applicantEmail (dataEmail)
-    errorMessage = validate_applicantEmail (dataEmail)
-    if errorMessage is not None:
-        messagebox.showinfo("Error", errorMessage)
-        return
-
-    #Contact number
-    dataContactNumber = entryContact.get()
-    dataContactNumber = standardise_applicantContactNumber (dataContactNumber)
-    errorMessage = validate_applicantContactNumber (dataContactNumber)
-    if errorMessage is not None:
-        messagebox.showinfo("Error", errorMessage)
-        return
-
-    #Name, position applied, language (written), language (spoken), programming language, past work experience, past work duration, highest education, soft skill
-    dataName = entryName.get()
-    dataPositionApplied = entryPosition.get()
-    dataLanguageWritten = entryLangWrite.get()
-    dataLanguageSpoken = entryLangSpoke.get()
-    dataProgrammingLanguage = entryCodeLang.get()
-    dataPastWorkExperience = entryWorkExp.get()
-    dataPastWorkDuration = entryDuration.get()
-    dataHighestEducation = entryHighestEdu.get()
-    dataSoftSkill = entrySoftSkill.get() #Still not sure about the "entrySoftSkill" name
-    errorMessage = validate_otherData (dataName, dataPositionApplied, dataLanguageWritten, dataLanguageSpoken, dataProgrammingLanguage, dataPastWorkExperience, dataPastWorkDuration, dataHighestEducation, dataSoftSkill)
-    if errorMessage is not None:
-        messagebox.showinfo("Error", errorMessage)
-        return
-
-    outMessage = None
-    
-
-    #Insert into database
-    import mysql.connector
-    #mysql.conector is a driver to access mySQL database.
-    #It is used to insert a new record into the database, and check whether the insertion of record was successful or not.
-    #Source: It is installed together when installing the mySQL software using mySQL installer. Source of the mySQL installer: https://dev.mysql.com/downloads/installer/
-    database = mysql.connector.connect (
-        host = "localhost",
-        user = "Resume_Scanning_Project_Admin",
-        password = "AbcdeF,123456!",
-        database = "ResumeScanningProject"
-    )
-    databaseCursor = database.cursor()
-    try:
-        returnMessage = databaseCursor.callproc ("InsertNewRecord", (dataICNumber, dataName, dataEmail, dataContactNumber, dataLanguageWritten, dataLanguageSpoken, dataProgrammingLanguage, dataPastWorkExperience, dataPastWorkDuration, dataHighestEducation, dataSoftSkill, dataPositionApplied, outMessage))
-        database.commit()
-        if returnMessage[12] == "Record successfully added":
-            messagebox.showinfo("", returnMessage[12])
-        else:
-            messagebox.showinfo("Error", returnMessage[12])
-    except mysql.connector.Error as errorMessage:
-        messagebox.showinfo("Error", errorMessage)
-    finally:
-        databaseCursor.close()
-        database.close()
-
+from ValidateData import standardise_applicantICNumber, validate_applicantICNumber, standardise_applicantEmail, validate_applicantEmail, standardise_applicantContactNumber, validate_applicantContactNumber, validate_otherData
+#ValidateData contains code for data validation.
+#It is used to make sure that the data is valid before it is inserted into the database.
+#Source: Part of this project
 
 
 #Window size, title
@@ -96,7 +22,6 @@ root.title("Add Record")
 #Define image for Background and button
 bg= PhotoImage(file='D:/GUI Image/background.png')
 SaveButtonImg =PhotoImage(file='D:/GUI Image/Save button.png')
-BackToMenuButtonImg =PhotoImage(file='D:/GUI Image/Back to menu.png')
 
 #Background
 my_canvas = Canvas(root,width=1920,height=1080)
@@ -110,7 +35,6 @@ Header2Font = Font(family="Montserrat Medium", size=30, weight="normal",slant="r
 Header3Font = Font(family="Montserrat Medium", size=15, weight="normal",slant="roman",underline=0,overstrike=0)
 FormFont = Font(family="Montserrat Medium", size=25, weight="normal",slant="roman",underline=0,overstrike=0)
 FillFont = Font(family="Montserrat Medium", size=20, weight="normal",slant="roman",underline=0,overstrike=0)
-
 
 #Creating Header
 Header = Canvas(root, width=1920,height=158,bg='#BAE2DC')
@@ -135,6 +59,7 @@ my_canvas.create_text(140,639,anchor="nw", text="Programming Language:", font=Fi
 my_canvas.create_text(140,694,anchor="nw", text="Past Work Experience:", font=FillFont)
 my_canvas.create_text(880,694,anchor="nw", text="Duration:", font=FillFont)
 my_canvas.create_text(140,749,anchor="nw", text="Highest Education:", font=FillFont)
+my_canvas.create_text(140,810,anchor="nw", text="Soft Skills:", font=FillFont)
 
 #Form fill
 entryIC = Entry(root, width = 100)
@@ -170,10 +95,76 @@ my_canvas.create_window(1040, 704, anchor="nw", window=entryDuration)
 entryHighestEdu = Entry(root, width = 100)
 my_canvas.create_window(500, 762, anchor="nw", window=entryHighestEdu)
 
+entrySoftSkills = Entry(root, width = 100)
+my_canvas.create_window(500, 820, anchor="nw", window=entrySoftSkills)
+
+#Function executed when Save button is pressed
+def insertNewRecord ():
+
+    """Function to insert new applicant's information into the database.
+       This function does not have specific input parameters, but the function will get the applicant's information from the entries.
+       Output is a message box that shows the message "Record successfully added" if the insertion was successful, or a message box that promts error message if the insertion was not successful."""
+
+    #Get data, validate data and set the variables
+    dataICNumber = entryIC.get()
+    dataICNumber = standardise_applicantICNumber (dataICNumber)
+    errorMessage = validate_applicantICNumber (dataICNumber)
+    if errorMessage is not None:
+        messagebox.showinfo("Error", errorMessage)
+        return
+    dataEmail = entryEmail.get()
+    dataEmail = standardise_applicantEmail (dataEmail)
+    errorMessage = validate_applicantEmail (dataEmail)
+    if errorMessage is not None:
+        messagebox.showinfo("Error", errorMessage)
+        return
+    dataContactNumber = entryContact.get()
+    dataContactNumber = standardise_applicantContactNumber (dataContactNumber)
+    errorMessage = validate_applicantContactNumber (dataContactNumber)
+    if errorMessage is not None:
+        messagebox.showinfo("Error", errorMessage)
+        return
+    dataName = entryName.get()
+    dataPositionApplied = entryPosition.get()
+    dataLanguageWritten = entryLangWrite.get()
+    dataLanguageSpoken = entryLangSpoke.get()
+    dataProgrammingLanguage = entryCodeLang.get()
+    dataPastWorkExperience = entryWorkExp.get()
+    dataPastWorkDuration = entryDuration.get()
+    dataHighestEducation = entryHighestEdu.get()
+    dataSoftSkill = entrySoftSkills.get()
+    errorMessage = validate_otherData (dataName, dataPositionApplied, dataLanguageWritten, dataLanguageSpoken, dataProgrammingLanguage, dataPastWorkExperience, dataPastWorkDuration, dataHighestEducation, dataSoftSkill)
+    if errorMessage is not None:
+        messagebox.showinfo("Error", errorMessage)
+        return
+    outMessage = None
+
+    #Insert into database
+    database = mysql.connector.connect (
+        host = "localhost",
+        user = "Resume_Scanning_Project_Admin",
+        password = "AbcdeF,123456!",
+        database = "ResumeScanningProject"
+    )
+    databaseCursor = database.cursor()
+    try:
+        returnMessage = databaseCursor.callproc ("InsertNewRecord", (dataICNumber, dataName, dataEmail, dataContactNumber, dataLanguageWritten, dataLanguageSpoken, dataProgrammingLanguage, dataPastWorkExperience, dataPastWorkDuration, dataHighestEducation, dataSoftSkill, dataPositionApplied, outMessage))
+        database.commit()
+        if returnMessage[12] == "Record successfully added":
+            messagebox.showinfo("", returnMessage[12])
+        else:
+            messagebox.showinfo("Error", returnMessage[12])
+    except mysql.connector.Error as errorMessage:
+        messagebox.showinfo("Error", errorMessage)
+    finally:
+        databaseCursor.close()
+        database.close()
+
 #Save Button
 img_label = Label(image=SaveButtonImg)
 entryButtonSave = Button(root, image=SaveButtonImg, borderwidth=0, highlightthickness=0, bd=0, command=insertNewRecord)
 my_canvas.create_window(140, 803, anchor="nw", window=entryButtonSave)
+
 
 
 root.mainloop()
